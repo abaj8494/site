4b434722c30a6aa1cc36242af2e5aebe34f8b5f3
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-02-17 17:37:46 +1100

    knn ocr

---
+++ content/projects/ml/supervised/classification/knn/mfnist/index.py
@@ -0,0 +1,10 @@
+DATA_DIR = 'data/'
+TEST_DATA_FILENAME = DATA_DIR + 't10k-images-idx3-ubyte'
+TEST_LABELS_FILNEAME = DATA_DIR + 't10k-labels-idx1-ubyte'
+TRAIN_DATA_FILENAME = DATA_DIR + 'train-images-idx3-ubyte'
+TRAIN_LABELS_FILENAME = DATA_DIR + 'train-labels-idx1-ubyte'
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
