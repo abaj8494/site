706edb78a3925ed184fa6877c95a5b21c5d3a7d2
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-07-25 07:11:01 +1000

    deleted feed-old, moving projects to work

--- static/js/nav-header.js
+++ static/js/nav-header.js
@@ -92,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Define target selectors for your navigation links.
     const targets = [
-      { selector: 'nav#menu a[href="/projects/"]' },
+      { selector: 'nav#menu a[href="/work/"]' },
       { selector: 'nav#menu a[href="/words/"]' },
       { selector: 'nav#menu a[href="/blog/"]' },
       { selector: 'nav#menu a[href="/about/"]' },
